1944803bc3c98f5737f7192acda9221197fda9c8
David Hristov <113634059+hrisco555@users.noreply.github.com>
2023-07-07 16:38:18 +0200

    Create 20.c

---
+++ Labs/Zadaci za vezbanje od labs-2 kol/20.c
@@ -0,0 +1,49 @@
+/*
+Да се прочита број N од тастатура. Да се дефинира функција form(int n) што ќе испечати форма од ѕвезди како што е прикажано на сликата.
+
+За да се освојат сите поени од вежбата, потребно е функцијата form да биде рекурзивна  
+
+5
+*****
+****
+***
+**
+*
+
+*/
+
+#include <stdio.h>
+
+void pechati_zvezda(int n, char znak){
+    
+    if(n==0){
+        printf("\n");
+        return;
+    }
+    
+    printf("%c", znak);
+    pechati_zvezda(n-1, znak);
+    
+}
+
+
+void form(int n){
+    
+    if(n==0){
+        return;
+    }
+    
+    if(n>0){
+        pechati_zvezda(n,'*');
+    }
+    form(n-1);
+}
+
+int main(){
+    
+    int n;
+    scanf("%d", &n);
+    form(n);
+    
+    return 0;
+}
